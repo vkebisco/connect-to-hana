335b110288bb8c74932f087747d2be3c7bc5cad0
vkrepsky <victorwkrepsky@gmail.com>
2023-01-19 14:51:43 -0300

    removidos imports inuteis

--- connectToHanaSpringBoot/src/main/java/com/example/demo/resources/UserService.java
+++ connectToHanaSpringBoot/src/main/java/com/example/demo/resources/UserService.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.naming.AuthenticationException;
-import java.util.List;
 import java.util.Objects;
 
 @RestController
